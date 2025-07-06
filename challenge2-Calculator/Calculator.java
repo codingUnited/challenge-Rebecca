c1e62c395530333cd39c78a99fff1120ad9d5eb1
Rebecca DuPont <RElinich@gmail.com>
2025-07-06 14:40:23 -0400

    Update
    Background on site updated. Java Calc & Challenge 2 added

---
+++ challenge2-Calculator/Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Coding United Challenge 2:
+ *
+ */
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Basic core calc:
+        System.out.print("Enter a number: ");
+        double num1 = input.nextDouble();
+        System.out.print("Enter a number: ");
+        double num2 = input.nextDouble();
+
+    
+
+
+        input.close();
+    }
+}
