55b6d90c72c995bf8fe1f8e1e39b39b3dd2dc878
Rebecca DuPont <RElinich@gmail.com>
2025-07-08 06:36:45 -0400

    Challenge 2 - Updated
    Core Calculator elements laid out and put into place.

--- challenge2-Calculator/Calculator.java
+++ challenge2-Calculator/Calculator.java
@@ -7,16 +7,44 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean quit = false;
 
-        //Basic core calc:
-        System.out.print("Enter a number: ");
-        double num1 = input.nextDouble();
-        System.out.print("Enter a number: ");
-        double num2 = input.nextDouble();
+            //Basic core calc:
+            System.out.print("Enter a number: ");
+            double num1 = input.nextDouble();
+            System.out.print("Enter a number: ");
+            double num2 = input.nextDouble();
 
-    
+            System.out.print("Select your operation (+, -, *, /): ");
+            String operation = input.next();
 
-
+            switch (operation) {
+                case "+":
+                    double result = num1 + num2;
+                    System.out.println(result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    System.out.println(result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    System.out.println(result);
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.print("You cannot divide by zero\n");
+                    } else {
+                        result = num1 / num2;
+                        System.out.println(result);
+                    }
+                    break;
+                default:
+                    System.out.print("That is not a valid input. Please input numerical digits.");
+            }
+        System.out.print("Calculation completed.");
         input.close();
     }
 }
+
+
