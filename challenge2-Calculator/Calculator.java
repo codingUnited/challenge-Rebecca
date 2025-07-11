6b29e2e1bd0cc83b3b438d7a80773d815327a441
Rebecca DuPont <RElinich@gmail.com>
2025-07-10 20:49:26 -0400

    Error handling and further dev
    Added try/catch for incorrect inputs, beginning to flesh out to prepare for class development.

--- challenge2-Calculator/Calculator.java
+++ challenge2-Calculator/Calculator.java
@@ -1,26 +1,41 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  * Coding United Challenge 2:
- *
+ * Calculator
  */
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean quit = false;
+        boolean quit = false; //loop controller
+        boolean correctValue = false; //used for try/catch to handle incorrect data type inputs
+        double num1 = 0;
+        double num2 = 0;
+        double result;//switch variable
 
-            //Basic core calc:
-            System.out.print("Enter a number: ");
-            double num1 = input.nextDouble();
-            System.out.print("Enter a number: ");
-            double num2 = input.nextDouble();
+        //Basic core calc:
+        //Clean up to handle two numbers entered in one line:
+       do {
+            try {
+                System.out.print("Enter a number: ");
+                num1 = input.nextDouble();
+                System.out.print("Enter a number: ");
+                num2 = input.nextDouble();
+                correctValue = true;
+            }catch (InputMismatchException e){
+                System.out.print("That is not a valid input. Please input numerical digits.\n");
+                input.next();
+            }
+        } while(!correctValue);
 
             System.out.print("Select your operation (+, -, *, /): ");
             String operation = input.next();
+            //Calculator core starting point:
 
             switch (operation) {
                 case "+":
-                    double result = num1 + num2;
+                    result = num1 + num2;
                     System.out.println(result);
                     break;
                 case "-":
@@ -31,19 +46,22 @@ public class Calculator {
                     result = num1 * num2;
                     System.out.println(result);
                     break;
-                case "/":
+                case "/": //Handles for division by 0
                     if (num2 == 0) {
-                        System.out.print("You cannot divide by zero\n");
+                        System.out.print("You cannot divide by zero\n"); //Return user to repeat input numbers (Add in)
                     } else {
                         result = num1 / num2;
                         System.out.println(result);
                     }
                     break;
-                default:
+                default: //Do I really need default?
                     System.out.print("That is not a valid input. Please input numerical digits.");
             }
+
         System.out.print("Calculation completed.");
         input.close();
+
+
     }
 }
 
